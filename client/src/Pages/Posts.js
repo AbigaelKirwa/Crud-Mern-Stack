378d0a585f90037f6469b455685f9323bdb64608
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2023-06-22 15:16:07 +0300

    create working read not

---
+++ client/src/Pages/Posts.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react"
+import axios from "axios"
+
+function Posts(){
+    useEffect(()=>{
+        axios.get("/posts")
+        .then((res)=> console.log(res))
+        .catch((err)=>console.log(err));
+    },[])
+    return(
+        <div style={{width:"90%", textAlign:"center", margin:"auto auto"}}>
+            <h1>Posts page</h1>
+        </div>
+    )
+}
+
+export default Posts
